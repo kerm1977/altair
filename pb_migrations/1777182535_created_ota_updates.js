4ac3d74b696b47abbaaccba1189befb1e2049df7
bncr1234 <kenth1977@gmail.com>
2026-04-26 10:54:00 -0600

    agregado el sistema de actualuzacion con tailscale y pocketbase integrado y conectando y OTA

---
+++ pb_migrations/1777182535_created_ota_updates.js
@@ -0,0 +1,65 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = new Collection({
+    "createRule": null,
+    "deleteRule": null,
+    "fields": [
+      {
+        "autogeneratePattern": "[a-z0-9]{15}",
+        "hidden": false,
+        "id": "text3208210256",
+        "max": 15,
+        "min": 15,
+        "name": "id",
+        "pattern": "^[a-z0-9]+$",
+        "presentable": false,
+        "primaryKey": true,
+        "required": true,
+        "system": true,
+        "type": "text"
+      },
+      {
+        "autogeneratePattern": "",
+        "hidden": false,
+        "id": "text3206337475",
+        "max": 0,
+        "min": 0,
+        "name": "version",
+        "pattern": "",
+        "presentable": false,
+        "primaryKey": false,
+        "required": true,
+        "system": false,
+        "type": "text"
+      },
+      {
+        "hidden": false,
+        "id": "file2558852872",
+        "maxSelect": 0,
+        "maxSize": 0,
+        "mimeTypes": null,
+        "name": "archivo_zip",
+        "presentable": false,
+        "protected": false,
+        "required": true,
+        "system": false,
+        "thumbs": null,
+        "type": "file"
+      }
+    ],
+    "id": "pbc_3099749703",
+    "indexes": [],
+    "listRule": "",
+    "name": "ota_updates",
+    "system": false,
+    "type": "base",
+    "updateRule": null,
+    "viewRule": ""
+  });
+
+  return app.save(collection);
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("pbc_3099749703");
+
+  return app.delete(collection);
+})
